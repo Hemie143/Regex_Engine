6106da4e0550bf5ba8f89ce144881d26513bea4d
hemie143 <hemie143@gmail.com>
2020-11-28 22:13:42 +0100

    Stage 2

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,11 +1,20 @@
 
-def match(regex, text):
-    if regex == text or regex == '.' or regex == '':
+def matchchar(regexchar, char):
+    if regexchar == char or regexchar == '.' or regexchar == '':
         return True
-    elif text == '':
+    elif char == '':
         pass
     return False
 
-regex, text = input().split('|')
+def matchstring(regex, text):
+    if len(regex) == 0:
+        return True
+    if len(text) == 0:
+        return False
+    if matchchar(regex[0], text[0]):
+        return matchstring(regex[1:], text[1:])
+    else:
+        return False
 
-print(match(regex, text))
+regex, text = input().split('|')
+print(matchstring(regex, text))
